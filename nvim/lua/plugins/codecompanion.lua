b9a874e2ef5ae08d6cb97e66a42be43c2f39eb02
Peter Filimonov <p.filimonov@geneaero.com>
2026-02-08 21:55:46 -0300

    codecompanion

---
+++ nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,16 @@
+return {
+  "olimorris/codecompanion.nvim",
+  version = "^18.0.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    interactions = {
+      chat = {
+        adapter = "anthropic",
+        model = "claude-sonnet-4-5",
+      },
+    },
+  },
+}
