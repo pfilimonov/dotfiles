e3a6b26e6b2bb7367f55943b63fdd22ada78e47a
Peter Filimonov <p.filimonov@geneaero.com>
2026-02-13 18:26:09 -0300

    new colorschemes and vscode as default

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,48 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
-  -- Отключи gruvbox (или просто удали его блок)
-  -- { "ellisonleao/gruvbox.nvim", enabled = false },
 
-  -- Новая тема cyberdream
   {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       require("cyberdream").setup({
-        variant = "light", -- или "default" для темной, "auto" для авто
+        variant = "auto", -- или "default" для темной, "auto" для авто
         transparent = false,
         -- Другие опции из документации можно добавить тут
       })
-      vim.cmd.colorscheme("cyberdream")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "day", -- для светлой версии
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "latte", -- latte, frappe, macchiato, mocha
+      })
+    end,
+  },
+  {
+    "mofiqul/vscode.nvim",
+    name = "vscode",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vscode").setup({
+        style = "light",
+      })
     end,
   },
 }
